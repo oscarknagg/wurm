4e54a1f24264d02b3d5e588569d5a449c63c7902
Oscar Knagg <oscar@knagg.co.uk>
2019-07-05 21:53:59 +0100

    Reorganised laser tag tests

--- tests/test_laser_tag.py
+++ tests/test_laser_tag.py
@@ -11,7 +11,7 @@ from tests._laser_trajectories import expected_laser_trajectories_0_2, expected_
 from config import DEFAULT_DEVICE
 
 
-render_envs = False
+render_envs = True
 size = 9
 render_sleep = 0.4
 # render_sleep = 1
@@ -54,39 +54,41 @@ def render(env):
         sleep(render_sleep)
 
 
-class TestLaserTag(unittest.TestCase):
-    def _test_action_sequence(self, env, all_actions, expected_orientations=None, expected_x=None, expected_y=None,
-                              expected_hp=None, expected_reward=None, expected_done=None):
+def test_action_sequence(self, env, all_actions, expected_orientations=None, expected_x=None, expected_y=None,
+                          expected_hp=None, expected_reward=None, expected_done=None):
+    render(env)
+
+    for i in range(all_actions['agent_0'].shape[0]):
+        actions = {
+            agent: agent_actions[i] for agent, agent_actions in all_actions.items()
+        }
+
+        obs, rewards, dones, info = env.step(actions)
         render(env)
+        env.check_consistency()
 
-        for i in range(all_actions['agent_0'].shape[0]):
-            actions = {
-                agent: agent_actions[i] for agent, agent_actions in all_actions.items()
-            }
+        if expected_x is not None:
+            self.assertTrue(torch.equal(env.x.cpu(), expected_x[i]))
+        if expected_y is not None:
+            self.assertTrue(torch.equal(env.y.cpu(), expected_y[i]))
+        if expected_orientations is not None:
+            self.assertTrue(torch.equal(env.orientations.cpu(), expected_orientations[i]))
+        if expected_hp is not None:
+            self.assertTrue(torch.equal(env.hp.cpu(), expected_hp[i]))
+        if expected_reward is not None:
+            self.assertTrue(torch.equal(env.rewards.cpu(), expected_reward[i]))
+        if expected_done is not None:
+            self.assertTrue(torch.equal(env.dones.cpu(), expected_done[i]))
 
-            obs, rewards, dones, info = env.step(actions)
-            render(env)
-            env.check_consistency()
-            print()
+        env.reset()
 
-            if expected_x is not None:
-                self.assertTrue(torch.equal(env.x.cpu(), expected_x[i]))
-            if expected_y is not None:
-                self.assertTrue(torch.equal(env.y.cpu(), expected_y[i]))
-            if expected_orientations is not None:
-                self.assertTrue(torch.equal(env.orientations.cpu(), expected_orientations[i]))
-            if expected_hp is not None:
-                self.assertTrue(torch.equal(env.hp.cpu(), expected_hp[i]))
-            if expected_reward is not None:
-                self.assertTrue(torch.equal(env.rewards.cpu(), expected_reward[i]))
-            if expected_done is not None:
-                self.assertTrue(torch.equal(env.dones.cpu(), expected_done[i]))
 
+class TestLaserTagSmall2(unittest.TestCase):
     def test_random_actions(self):
         """Tests a very large number of random actions and checks for environment consistency
         instead of any particular expected trajectory."""
         num_envs = 128
-        num_steps = 256
+        num_steps = 512
         num_agents = 2
         obs_fn = observations.FirstPersonCrop(
             first_person_rotation=True,
@@ -104,7 +106,7 @@ class TestLaserTag(unittest.TestCase):
             range(num_agents)
         }
 
-        self._test_action_sequence(env, all_actions)
+        test_action_sequence(env, all_actions)
 
     def test_basic_movement(self):
         """2 agents rotate completely on the spot then move in a circle."""
@@ -127,7 +129,7 @@ class TestLaserTag(unittest.TestCase):
             [7, 7, 7, 7, 7, 7, 6, 6, 6, 6, 7],
         ]).t()
 
-        self._test_action_sequence(env, all_actions, expected_orientations, expected_x, expected_y)
+        test_action_sequence(env, all_actions, expected_orientations, expected_x, expected_y)
 
     def test_move_backward(self):
         env = get_test_env(num_envs=1)
@@ -148,7 +150,7 @@ class TestLaserTag(unittest.TestCase):
             [2, 2],
         ]).t()
 
-        self._test_action_sequence(env, all_actions, expected_orientations, expected_x, expected_y)
+        test_action_sequence(env, all_actions, expected_orientations, expected_x, expected_y)
 
     def test_strafe(self):
         env = get_test_env(num_envs=1)
@@ -169,7 +171,7 @@ class TestLaserTag(unittest.TestCase):
             [2, 2],
         ]).t()
 
-        self._test_action_sequence(env, all_actions, expected_orientations, expected_x, expected_y)
+        test_action_sequence(env, all_actions, expected_orientations, expected_x, expected_y)
 
     def test_wall_pathing(self):
         """Test agents can't walk through walls."""
@@ -187,7 +189,7 @@ class TestLaserTag(unittest.TestCase):
             [7, 7, 7, 7, 7, 6, 6]
         ]).t()
 
-        self._test_action_sequence(env, all_actions, None, expected_x, expected_y)
+        test_action_sequence(env, all_actions, None, expected_x, expected_y)
 
     def test_move_onto_other_agent(self):
         """Test an agent can't move onto another agent."""
@@ -197,7 +199,11 @@ class TestLaserTag(unittest.TestCase):
             'agent_1': torch.tensor([0, 2, 3, 3, 3, 3, 3, 3, 3]).unsqueeze(1).long().to(DEFAULT_DEVICE),
         }
 
-        self._test_action_sequence(env, all_actions)
+        test_action_sequence(env, all_actions)
+
+    @pytest.mark.skip()
+    def test_move_through_other_agent(self):
+        pass
 
     def test_agents_move_into_same_square(self):
         """Test two agent can't simultaneously choose to move onto the same square."""
@@ -207,7 +213,7 @@ class TestLaserTag(unittest.TestCase):
             'agent_1': torch.tensor([2, 3, 3, 3, 3, 3, 3, 3]).unsqueeze(1).long().to(DEFAULT_DEVICE),
         }
 
-        self._test_action_sequence(env, all_actions)
+        test_action_sequence(env, all_actions)
 
     def test_firing_orientations_0_2(self):
         """Tests that laser trajectories are calculated correctly."""
@@ -248,7 +254,7 @@ class TestLaserTag(unittest.TestCase):
             self.assertTrue(torch.equal(env.lasers, expected_laser_trajectories_1_3[i]))
             render(env)
 
-    def test_being_hit(self):
+    def test_being_hit_orientations_0_2(self):
         """Tests that agent-laser collision is calculated correctly, hp is deducted
         and reward is given."""
         env = get_test_env(num_envs=1)
@@ -269,7 +275,31 @@ class TestLaserTag(unittest.TestCase):
             [0, ] * (all_actions['agent_0'].shape[0] - 1) + [1],
         ]).t().byte()
 
-        self._test_action_sequence(env, all_actions, expected_hp=expected_hp, expected_reward=expected_reward,
+        test_action_sequence(env, all_actions, expected_hp=expected_hp, expected_reward=expected_reward,
+                                   expected_done=expected_done)
+
+    def test_being_hit_orientations_1_3(self):
+        """Tests that agent-laser collision is calculated correctly, hp is deducted
+        and reward is given."""
+        env = get_test_env(num_envs=1)
+        all_actions = {
+            'agent_0': torch.tensor([2, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7]).unsqueeze(1).long().to(DEFAULT_DEVICE),
+            'agent_1': torch.tensor([3, 3, 3, 3, 3, 3, 2, 0, 0, 7, 0]).unsqueeze(1).long().to(DEFAULT_DEVICE),
+        }
+        expected_hp = torch.tensor([
+            [2, ] * (all_actions['agent_0'].shape[0] - 3) + [2, 1, 1],
+            [2, ] * (all_actions['agent_0'].shape[0] - 3) + [1, 1, 0],
+        ]).t()
+        expected_reward = torch.tensor([
+            [0, ] * (all_actions['agent_0'].shape[0] - 3) + [1, 0, 1],
+            [0, ] * (all_actions['agent_0'].shape[0] - 3) + [0, 1, 0],
+        ]).t().float()
+        expected_done = torch.tensor([
+            [0, ] * all_actions['agent_0'].shape[0],
+            [0, ] * (all_actions['agent_0'].shape[0] - 1) + [1],
+        ]).t().byte()
+
+        test_action_sequence(env, all_actions, expected_hp=expected_hp, expected_reward=expected_reward,
                                    expected_done=expected_done)
 
     def test_cant_shoot_through_agents(self):
@@ -313,14 +343,13 @@ class TestLaserTag(unittest.TestCase):
 
             render(env)
 
-    def test_asymettric_map(self):
-        # env = LaserTag(num_envs=1, num_agents=2, height=9, width=16, map_generator=Small3(DEFAULT_DEVICE),
-        #                device=DEFAULT_DEVICE)
-        env = LaserTag(num_envs=1, num_agents=4, height=14, width=22, map_generator=Small4(DEFAULT_DEVICE),
-                       device=DEFAULT_DEVICE)
-        if render_envs:
-            env.render()
-            sleep(5)
+    def test_all_die_simultaneously(self):
+        env = get_test_env(num_envs=1)
+        all_actions = {
+            'agent_0': torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 0]).unsqueeze(1).long().to(DEFAULT_DEVICE),
+            'agent_1': torch.tensor([0, 2, 3, 3, 3, 3, 3, 3, 1, 7, 7, 0]).unsqueeze(1).long().to(DEFAULT_DEVICE),
+        }
+        test_action_sequence(env, all_actions)
 
     def test_observations(self):
         obs_fn = observations.RenderObservations()
@@ -338,12 +367,12 @@ class TestLaserTag(unittest.TestCase):
     def test_partial_observations(self):
         obs_fn = observations.FirstPersonCrop(
             first_person_rotation=True,
-            in_front=9,
+            in_front=11,
             behind=2,
-            side=4,
+            side=6,
             padding_value=127
         )
-        env = get_test_env(num_envs=1)
+        env = get_test_env(num_envs=2)
 
         agent_obs = obs_fn.observe(env)
         for agent, obs in agent_obs.items():
@@ -361,6 +390,7 @@ class TestLaserTag(unittest.TestCase):
         env = LaserTag(num_envs=16, num_agents=2, height=9, width=9,
                        map_generator=Small2(DEFAULT_DEVICE), observation_fn=obs_fn,
                        device=DEFAULT_DEVICE)
+        env.check_consistency()
 
     def test_render(self):
         env = get_test_env()
@@ -368,3 +398,32 @@ class TestLaserTag(unittest.TestCase):
         if render_envs:
             env.render()
             sleep(5)
+
+
+class TestSmall3(unittest.TestCase):
+    def test_asymettric_map(self):
+        env = LaserTag(num_envs=1, num_agents=2, height=9, width=16, map_generator=Small3(DEFAULT_DEVICE),
+                       device=DEFAULT_DEVICE, colour_mode='fixed')
+
+        env.agents = torch.zeros_like(env.agents)
+        env.agents[0, 0, 1, 1] = 1
+        env.agents[1, 0, -2, -2] = 1
+        env.orientations[0] = 3
+        env.orientations[1] = 1
+
+        all_actions = {
+            'agent_0': torch.tensor([7, 0, 7, 0, 7, 0]).unsqueeze(1).long().to(DEFAULT_DEVICE),
+            'agent_1': torch.tensor([0, 7, 0, 7, 0, 0]).unsqueeze(1).long().to(DEFAULT_DEVICE),
+        }
+
+        render(env)
+
+        for i in range(all_actions['agent_0'].shape[0]):
+            actions = {
+                agent: agent_actions[i] for agent, agent_actions in all_actions.items()
+            }
+
+            observations, rewards, dones, info = env.step(actions)
+            # Laser trajectories were verified manually then saved to another file because they are very verbose
+            # self.assertTrue(torch.equal(env.lasers, expected_laser_trajectories_1_3[i]))
+            render(env)
